ce7862143c6269ce88e420e63bef2db0a8449a37
fiopwk <flopwiki@gmail.com>
2019-12-27 18:34:45 -0500

    App.js: added Hooks and imported button module

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,21 +1,21 @@
 import React, {useState} from "react";
 
 //import any components needed
-import { numbers } from "../../../data";
+import NumberButton from "./NumberButton";
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
 import { numbers } from '../../../data' 
 */
 //Import your array data to from the provided data file
+import { numbers } from "../../../data";
 
-const Numbers = () => {
+const Numbers = (props, index) => {
   // STEP 2 - add the imported data to state
-  const [number, setNumber] = useState(numbers)
+  const [number, setNumber] = useState(numbers);
+  
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+{number.map(numb => { return <NumberButton value={numb} key={numb} putNumber = {props.putNumber} /> })}
     </div>
   );
 };
