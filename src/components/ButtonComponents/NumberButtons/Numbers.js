b81a493328f4b9b9142fcc7a06c06532cf66c74c
fiopwk <flopwiki@gmail.com>
2019-12-27 17:31:13 -0500

    data.js: exported modules per step 1

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
+import { numbers } from "../../../data";
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
 import { numbers } from '../../../data' 
@@ -9,6 +10,7 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [number, setNumber] = useState(numbers)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
@@ -17,3 +19,5 @@ const Numbers = () => {
     </div>
   );
 };
+
+export default Numbers;
